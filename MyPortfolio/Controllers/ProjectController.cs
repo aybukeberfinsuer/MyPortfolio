d414e0983e54cda8a7996c42c9d326997d2ec358
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-10-31 22:48:47 +0300

    1.1

---
+++ MyPortfolio/Controllers/ProjectController.cs
@@ -0,0 +1,20 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        MyPortfolioEntities db= new MyPortfolioEntities();
+
+        public ActionResult Index()
+        {
+            var projects = db.TblProjects.ToList();
+            return View(projects);
+        }
+    }
+}
