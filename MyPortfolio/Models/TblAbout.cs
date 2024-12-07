64dca33978b6b5b028b64b8f7f43638617aa0f62
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-12-07 16:16:33 +0300

    Revert "duzeltmeler"
    This reverts commit 62a85a75a59a601f70a0ae1eea92df2493181225.

--- MyPortfolio/Models/TblAbout.cs
+++ MyPortfolio/Models/TblAbout.cs
@@ -11,13 +11,19 @@ namespace MyPortfolio.Models
 {
     using System;
     using System.Collections.Generic;
-    
-    public partial class TblAbout
-    {
-        public int AboutId { get; set; }
-        public string ImageUrl { get; set; }
+	using System.ComponentModel.DataAnnotations.Schema;
+	using System.Web;
+
+	public partial class TblAbout
+	{
+		public int AboutId { get; set; }
+		public string ImageUrl { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string CvUrl { get; set; }
-    }
+		[NotMapped]  // Bu alan veritabanýna kaydedilmez
+		public HttpPostedFileBase ImageFile { get; set; }
+		[NotMapped]  // Bu alan veritabanýna kaydedilmez
+		public HttpPostedFileBase CvFile { get; set; }
+	}
 }
